231e2f9371aafa88179c346036f05cd0fbc55f98
Dechomp <julio.sougosan@gmail.com>
2026-04-23 11:46:10 -0300

    Perguntas do quiz de pt e mt corrigidas

---
+++ Gamestudando React/Gamestudando/app/perguntasMatematicaQuiz4.tsx
@@ -0,0 +1,77 @@
+export const perguntasMatematica = [
+  {
+    pergunta: "Quanto é 2 + 1?",
+    respostas: ["3", "1", "4", "2"],
+    correta: 0
+  },
+  {
+    pergunta: "Quanto é 5 - 2?",
+    respostas: ["5", "2", "4", "3"],
+    correta: 3
+  },
+  {
+    pergunta: "Quanto é 10 + 5?",
+    respostas: ["10", "20", "5", "15"],
+    correta: 3
+  },
+  {
+    pergunta: "Quanto é 8 - 3?",
+    respostas: ["6", "5", "4", "3"],
+    correta: 1
+  },
+  {
+    pergunta: "Qual número é MAIOR?",
+    respostas: ["1", "3", "9", "2"],
+    correta: 2
+  },
+  {
+    pergunta: "Quanto é 7 + 2?",
+    respostas: ["9", "8", "7", "10"],
+    correta: 0
+  },
+  {
+    pergunta: "Quanto é 6 - 1?",
+    respostas: ["4", "6", "3", "5"],
+    correta: 3
+  },
+  {
+    pergunta: "Quanto é 10 - 4?",
+    respostas: ["7", "4", "6", "5"],
+    correta: 2
+  },
+  {
+    pergunta: "Quanto é 3 + 3?",
+    respostas: ["6", "5", "7", "4"],
+    correta: 0
+  },
+  {
+    pergunta: "Quanto é 9 - 2?",
+    respostas: ["7", "6", "8", "5"],
+    correta: 0
+  },
+  {
+    pergunta: "Qual número é MENOR?",
+    respostas: ["8", "5", "9", "2"],
+    correta: 3
+  },
+  {
+    pergunta: "Quanto é 4 + 4?",
+    respostas: ["7", "8", "6", "9"],
+    correta: 1
+  },
+  {
+    pergunta: "Quanto é 7 - 3?",
+    respostas: ["4", "3", "5", "6"],
+    correta: 0
+  },
+  {
+    pergunta: "Quanto é 1 + 6?",
+    respostas: ["6", "8", "5", "7"],
+    correta: 3
+  },
+  {
+    pergunta: "Quanto é 5 + 5?",
+    respostas: ["11", "10", "9", "8"],
+    correta: 1
+  }
+];
---
+++ Gamestudando React/Gamestudando/app/perguntasPortuguesQuiz4.tsx
@@ -0,0 +1,77 @@
+export const perguntasPortugues = [
+  {
+    pergunta: "QUAL PALAVRA COMEÇA COM B?",
+    respostas: ["GATO", "CASA", "BOLA", "PATO"],
+    correta: 2
+  },
+  {
+    pergunta: "QUAL PALAVRA RIMA COM GATO?",
+    respostas: ["PATO", "CASA", "BOLA", "DEDO"],
+    correta: 0
+  },
+  {
+    pergunta: "QUAL COMEÇA COM CA?",
+    respostas: ["BOLA", "FOGO", "DADO", "CASA"],
+    correta: 3
+  },
+  {
+    pergunta: "QUAL TERMINA COM TO?",
+    respostas: ["MESA", "BOLA", "GATO", "CASA"],
+    correta: 2
+  },
+  {
+    pergunta: "BO-LA TEM QUANTAS SÍLABAS?",
+    respostas: ["1", "3", "4", "2"],
+    correta: 3
+  },
+  {
+    pergunta: "QUAL COMEÇA COM M?",
+    respostas: ["MESA", "PATO", "GATO", "CASA"],
+    correta: 0
+  },
+  {
+    pergunta: "QUAL RIMA COM CASA?",
+    respostas: ["ASA", "MESA", "GATO", "PATO"],
+    correta: 0
+  },
+  {
+    pergunta: "QUAL COMEÇA COM PA?",
+    respostas: ["CASA", "DEDO", "PATO", "BOLA"],
+    correta: 2
+  },
+  {
+    pergunta: "QUAL TERMINA COM SA?",
+    respostas: ["GATO", "PATO", "MESA", "BOLA"],
+    correta: 2
+  },
+  {
+    pergunta: "CA-SA TEM QUANTAS SÍLABAS?",
+    respostas: ["2", "1", "3", "4"],
+    correta: 0
+  },
+  {
+    pergunta: "QUAL PALAVRA COMEÇA COM G?",
+    respostas: ["BOLA", "CASA", "MESA", "GATO"],
+    correta: 3
+  },
+  {
+    pergunta: "QUAL RIMA COM PATO?",
+    respostas: ["MESA", "CASA", "GATO", "BOLA"],
+    correta: 2
+  },
+  {
+    pergunta: "QUAL COMEÇA COM D?",
+    respostas: ["GATO", "DADO", "CASA", "BOLA"],
+    correta: 1
+  },
+  {
+    pergunta: "DA-DO TEM QUANTAS SÍLABAS?",
+    respostas: ["1", "2", "3", "4"],
+    correta: 1
+  },
+  {
+    pergunta: "QUAL TERMINA COM LA?",
+    respostas: ["GATO", "BOLA", "CASA", "PATO"],
+    correta: 1
+  }
+];
